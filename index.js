bbc72fd2f8c644e29908a205775901d0e7e2e7e4
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-03-25 23:24:12 +0530

    feat: Enable real-time binary stream transfer with Socket.IO
    feat: Enable real-time binary stream transfer with Socket.IO
    
    This commit introduces Socket.IO for real-time communication:
    
    - Server-side:
        - Instantiates Socket.IO server
        - Logs client connections and disconnections
        - Handles incoming binary streams
        - Logs received binary data
    - Client-side:
        - Connects to Socket.IO server
        - Emits binary stream data on availability

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import http from "http";
 import express from "express";
 import path from "path";
+import { Server as socketIO } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new socketIO(server);
 
 app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("New client connected", socket.id);
+  socket.on("binarystream", (data) => {
+    console.log("Binary Stream Received", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
