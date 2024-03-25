7c0c573fc56993ff787761d5d015355f9cdcd86c
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-03-25 23:06:46 +0530

    feat: Add video recording functionality using MediaRecorder API
    feat: Add video recording functionality using MediaRecorder API
    
    This commit introduces code for video recording with key features:
    
    - User-initiated recording with a start button
    - MediaRecorder setup with audio/video configuration (128 kb/s, 25 fps)
    - Handling available data chunks (binary streams)
    - Accessing user media stream on page load
    - Displaying video in a designated HTML element

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from "http";
+import express from "express";
+import path from "path";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.resolve("./public")));
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ public/script.js
@@ -0,0 +1,29 @@
+const userVideo = document.getElementById('user-video');
+const startButton = document.getElementById('start-btn');
+
+const state = {media: null};
+
+startButton.addEventListener('click', () => {
+    const mediaRecorder = new MediaRecorder(state.media, {
+        audioBitsPerSecond: 128000,
+        videoBitsPerSecond: 128000,
+        fraamerate: 25
+        
+    });
+    mediaRecorder.ondataavailable = (e) => {
+        console.log("Binary Stream Available",e.data);
+    };
+
+    mediaRecorder.start(25);
+});
+
+
+window.addEventListener('load', async (e) => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    });
+    state.media = stream;
+    userVideo.srcObject = stream;
+});
+
